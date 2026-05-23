7fe0a8679930e638f452eda3a7292b74cf3d1943
nykollaschristofer3-crypto <nykollaschristofer3@gmail.com>
2026-05-23 08:43:11 -0300

    Delete atividade05_ifelse.py
    delet

--- atividade05_ifelse.py
+++
@@ -1,9 +0,0 @@
-#autor: Nykollas Christofer
-# Projeto:  desvio Condicional
-
-#criação das variáveis
-trbalhando = input('Você está trabalhando? (sim/não): ')
-if trbalhando.lower() == 'sim':
-    print('bom trabalho!')
-else:
-    print('desejo um bom dia!')
