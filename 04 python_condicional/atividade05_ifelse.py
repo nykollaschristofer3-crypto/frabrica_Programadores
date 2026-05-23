e44b6e2eaaeffcfabef8b526d26397c41282715a
nykollaschristofer3-crypto <nykollaschristofer3@gmail.com>
2026-05-23 08:42:13 -0300

    Add files via upload

---
+++ 04 python_condicional/atividade05_ifelse.py
@@ -0,0 +1,9 @@
+#autor: Nykollas Christofer
+# Projeto:  desvio Condicional
+
+#criação das variáveis
+trbalhando = input('Você está trabalhando? (sim/não): ')
+if trbalhando.lower() == 'sim':
+    print('bom trabalho!')
+else:
+    print('desejo um bom dia!')
