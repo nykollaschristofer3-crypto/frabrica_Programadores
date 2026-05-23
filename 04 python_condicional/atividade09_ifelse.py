9b06304aff525eb54abc58d455f7f41828506d92
nykollaschristofer3-crypto <nykollaschristofer3@gmail.com>
2026-05-23 08:41:30 -0300

    Add files via upload

---
+++ 04 python_condicional/atividade09_ifelse.py
@@ -0,0 +1,13 @@
+#autor: nykollas Christofer
+#projeto desvio condicional
+
+#criaçao de variavel
+temperatura = float(input("Digite a temperatura em celsius: "))
+if temperatura >= 30:
+    print("Está quente")
+elif temoeratura >= 20:
+    print("Está agradável")
+elif temperatura >= 10:
+    print("Está frio")
+else:
+    print("Está muito frio")
