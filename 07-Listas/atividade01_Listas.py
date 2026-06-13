223f24c5da4172548ea7dde48754c329adb2c71e
nykollaschristofer3-crypto <nykollaschristofer3@gmail.com>
2026-06-13 10:06:00 -0300

    Add files via upload

---
+++ 07-Listas/atividade01_Listas.py
@@ -0,0 +1,8 @@
+#autor = Nykollas Christofer
+# projeto: listas em python
+
+numeros = [1, 2, 3, 4, 5,6]
+nomes = ['pelé', 'maradona', 'messi', 'ronaldo']
+
+print(numeros)
+print(nomes)
---
+++ 07-Listas/atividade02_Listas.py
@@ -0,0 +1,10 @@
+#autor = nykollas christofer
+# projeto: listas em python
+
+numeros = [1, 2, 3, 4,5,6]
+nomes = ['joaquim', "maria", "pedro", "ana" "ruam"]
+print(nomes[0]) #imprime o nome joaquim
+print(nomes[1]) #imprime o nome maria
+print(nomes[2]) #imprime o nome pedro
+print(nomes[3]) #imprime o nome ana
+print(nomes[4]) #imprime o nome ruam
